2b8447628f051f8ce7160d19cce9afc7f14ee4b0
manisha-puri <manisha_puri31@yahoo.com>
2016-06-29 19:52:59 +0530

    updated

--- includes/js/script.js
+++ includes/js/script.js
@@ -3,4 +3,9 @@ $(function(){
     e.preventDefault();
     $('#successAlert').slideDown();
   });
+  $('a.pop').click(function(e){
+    e.preventDefault();
+  });
+  $('a.pop').popover();
+  $('[rel="tooltip"]').tooltip();
 });
